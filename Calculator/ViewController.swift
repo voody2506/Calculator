75e018fe1fbfbaaeb45ca370be571ed1e33e8d2e
voody2506 <voody2506@gmail.com>
2017-03-30 22:27:42 +0600

    first

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,7 +8,76 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var label: UILabel!
+    var memory: Int = 0
+    var action = " "
+
+    @IBAction func numbers(_ sender: Any) {
+        label.text = label.text! + String((sender as AnyObject).tag)
+    }
+    @IBAction func AC(_ sender: Any) {
+        label.text = ""
+    }
+    @IBAction func minus(_ sender: Any) {
+        memory = Int(label.text!)!
+        action = "minus"
+        label.text = ""
+    }
+    @IBAction func multiply(_ sender: Any) {
+        memory = Int(label.text!)!
+        action = "multiply"
+        label.text = ""
+    }
+    
+    @IBAction func divide(_ sender: Any) {
+        memory = Int(label.text!)!
+        action = "divide"
+        label.text = ""
+    }
+    
+    @IBAction func plus(_ sender: Any) {
+        memory = Int(label.text!)!
+        action = "plus"
+        label.text = ""
+    }
+    
+
+    @IBAction func answ(_ sender: Any) {
+        var answ = ""
+        var now = Int(label.text!)
+        if (action == "plus"){
+            
+             answ = String(memory + now!)
+            
+            label.text = answ
+        }
+        if (action == "divide"){
+            
+            answ = String(memory / now!)
+            
+            label.text = answ
+        }
+        
+        if (action == "minus"){
+            
+            answ = String(memory - now!)
+            
+            label.text = answ
+        }
+        if (action == "multiply"){
+            
+            answ = String(memory * now!)
+            
+            label.text = answ
+        }
+        
+        
+    }
+
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
